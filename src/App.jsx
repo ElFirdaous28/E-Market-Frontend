9601bad27f9fa0a8a16c144c98006e99adb40570
ElFirdaous28 <elmokhtarifirdaous@gmail.com>
2025-11-07 09:01:12 +0100

    EMF-10 dark mode

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,10 @@
-import './App.css'
+import DarkToggle from './darkToggel'
 
 function App() {
 
   return (
     <>
-      <h1 className="text-3xl font-bold underline bg-red-300">
-        Hello world!
-      </h1>
+    <DarkToggle></DarkToggle>
     </>
   )
 }
